625feb865a55e39fc37274621a358000a590095b
thanosme1074 <93836580+thanosme1074@users.noreply.github.com>
2021-12-24 09:58:24 +0500

    Update gLab_colab.py

--- gLab_colab.py
+++ gLab_colab.py
@@ -8,3 +8,8 @@ os.system('tar -xvzf ffuf*.tar.gz && rm CHANGELOG.md LICENSE README.md ffuf*.tar
 
 # Download and Install ngrok in google-colab
 os.system('curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list && sudo apt update && sudo apt install ngrok')
+
+# Install other packages
+os.system("""
+apt-get install nano file zip git poppler-utils php net-tools tar && pip install speedtest-cli wordlist
+""")
