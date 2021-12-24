268b1aa458475e45a1008080b179714d4a594a41
thanosme1074 <93836580+thanosme1074@users.noreply.github.com>
2021-12-24 09:52:31 +0500

    Update gLab_colab.py

--- gLab_colab.py
+++ gLab_colab.py
@@ -3,6 +3,8 @@ import requests, os
 url = """https://github.com/ffuf/ffuf/releases/download/v1.3.1/ffuf_1.3.1_linux_amd64.tar.gz"""
 with open(url.split('/')[-1], "wb+") as f:
     f.write(requests.Session().get(url).content)
+os.system('tar -xvzf ffuf*.tar.gz && rm CHANGELOG.md LICENSE README.md ffuf*.tar.gz && mv ffuf /bin')
+
 
 # Download and Install ngrok in google-colab
 os.system('curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list && sudo apt update && sudo apt install ngrok')
